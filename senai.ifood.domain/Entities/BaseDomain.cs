8b92a9d7bbc49730d09071688e00e1b84ee5e453
FIC <FIC@info.local>
2018-02-21 19:23:39 -0300

    iniciando aplicacação criando as classes

---
+++ senai.ifood.domain/Entities/BaseDomain.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace senai.ifood.domain.Entities
+{
+    public class BaseDomain
+    {
+        public int Id { get; set; }
+
+        public DateTime DataCriacao { get; set; }
+    }
+}
