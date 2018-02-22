6b5643851dd1068b71d43881d26dd5be31f78072
FIC <FIC@info.local>
2018-02-22 19:45:17 -0300

    criando banco de dados

---
+++ senai.ifood.repository/Context/IFoodContext.cs
@@ -0,0 +1,35 @@
+using Microsoft.EntityFrameworkCore;
+using senai.ifood.domain.Entities;
+
+namespace senai.ifood.repository.Context
+{
+    public class IFoodContext:DbContext
+    {
+        public IFoodContext(DbContextOptions<IFoodContext>options):base(options){
+
+        }
+        public DbSet <UsuarioDomain> Usuarios { get; set; }
+        public DbSet <ClienteDomain> Clientes { get; set; }
+
+        public DbSet <EspecialidadeDomain> Especialidades { get; set; }
+        public DbSet <PermissaoDomain> Permissoes { get; set; }
+            
+        public DbSet <ProdutoDomain> ProdutosRestaurantes { get; set; }
+        public DbSet <RestauranteDomain> Restaurantes { get; set; }
+
+        public DbSet <UsuarioPermissaoDomain> UsuariosPermissoes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder){
+            modelBuilder.Entity<ClienteDomain>().ToTable("Clientes");
+            modelBuilder.Entity<UsuarioDomain>().ToTable("Usuarios");
+            modelBuilder.Entity<EspecialidadeDomain>().ToTable("Especialidades");
+            modelBuilder.Entity<PermissaoDomain>().ToTable("Permissoes");
+            modelBuilder.Entity<ProdutoDomain>().ToTable("Produtos");
+            modelBuilder.Entity<RestauranteDomain>().ToTable("Restaurantes");
+            modelBuilder.Entity<UsuarioPermissaoDomain>().ToTable("UsuariosPermissoes");
+
+            base OnModelCreating(modelBuilder);
+
+        }
+    }
+}
