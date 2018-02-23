5c7da8d3872076f08a5056372a515a813a267abd
FIC <FIC@info.local>
2018-02-23 19:10:23 -0300

    implementando interface

--- senai.ifood.repository/Repositories/BaseRepository.cs
+++ senai.ifood.repository/Repositories/BaseRepository.cs
@@ -1,9 +1,34 @@
+using System.Collections.Generic;
 using senai.ifood.domain.Contracts;
+
 
 namespace senai.ifood.repository.Repositories
 {
     public class BaseRepository<T> : IBaseRepository<T> where T : class
     {
-        
+        public int Atualizar(T dados)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public T BuscarporId(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int Deletar(T dados)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int Inserir(T dados)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<T> Listar()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
